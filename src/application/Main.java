a04281a679ac19cae1d107da128358847f8dc528
Joaop014 <curicolopes@gmail.com>
2022-06-26 16:32:29 -0300

    main view controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,29 +1,30 @@
 package application;
-	
+
 import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
-
 
 public class Main extends Application {
 	@Override
-	public void start(Stage stage) {
-	try {
-	Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
-	 Scene scene = new Scene(parent);
-	 stage.setScene(scene);
-	 stage.show();
-	}
-	catch (IOException e) {
-	e.printStackTrace();
-	}
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/mainView.fxml"));
+			ScrollPane scrollPane = loader.load();
+			Scene mainScene = new Scene(scrollPane);
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Sample JavaFX application");
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
